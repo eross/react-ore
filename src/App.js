649dcfce5810500be13e3cea18c0f18994d0fa79
eross <eross@users.noreply.github.com>
2023-05-25 10:46:31 -0700

    Cleaned up bad refs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './App.css';
 
 function App() {
-  return <div>Let's get started.</div>;
+  return <div>Let's get started now.</div>;
 }
 
 export default App;
